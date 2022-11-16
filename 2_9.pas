deab6a860c789fb6a33336eadee4c334996de5ae
shameRRR <118363645+shameRRR@users.noreply.github.com>
2022-11-16 17:18:32 +0300

    Update 2_9.pas

--- 2_9.pas
+++ 2_9.pas
@@ -1,12 +1,13 @@
-﻿var
+var
   mas: array [1..20] of integer;
-  i, min: integer;
+  i,j, min: integer;
 
 begin
   for i := 1 to 20 do 
   begin
     mas[i] := random(150) - 50
   end;
+  writeln(mas);
   min := mas[1];
   for i := 2 to 20 do
     if mas[i] < min then
@@ -14,8 +15,11 @@ begin
       min := mas[i];
     end;
   for i := 1 to 20 do
-    if mas[i] > 0 then
-      break;
+      if mas[i] > 0 then
+        begin
+        mas[i]:=mas[i+1];
+        break;
+        end;
   
    writeln(min, mas, mas[i]);
 end.
